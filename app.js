2917aff7ee29bf168ad9732c846cde491a6902f7
Neethusanal <neethukumarkk@gmail.com>
2024-03-27 17:32:41 +0300

    loginpage

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 require('dotenv').config();
 const app=express()
+const hbs = require('hbs');
+
 const port= process.env.PORT 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
+})
+app.set('view engine', 'hbs');
+app.get('/login',(req ,res)=>{
+    // res.send("hello")
+    res.render("login")
+
 })
