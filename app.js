4aaa625edcdb431eb024ceec87820ac7d3cd19c4
Neethusanal <neethukumarkk@gmail.com>
2024-04-09 13:00:30 +0300

    signup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('dotenv').config();
 const app=express()
 const hbs = require('hbs');
+app.use(express.json())
 
 const port= process.env.PORT 
 app.listen(port,()=>{
@@ -13,4 +14,13 @@ app.get('/login',(req ,res)=>{
     // res.send("hello")
     res.render("login")
 
+})
+
+app.get('/signup',(req,res)=>{
+    
+    res.render("signup")
+})
+app.get('/',(req,res)=>{
+  
+    res.render("home")
 })
